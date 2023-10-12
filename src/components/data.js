4d81e90d9444e51a0414409ea74dcd67c82d013d
Kerem Karaman <keremkaraman91@gmail.com>
2023-10-12 17:32:03 +0300

    productCard optimized

--- src/components/data.js
+++ src/components/data.js
@@ -19,5 +19,25 @@ const productData = [
     content: "Graphic design",
     department: "English Department",
   },
+  {
+    url: "/src/assets/productCardImg/product-cover-5.png",
+    content: "Graphic design",
+    department: "English Department",
+  },
+  {
+    url: "/src/assets/productCardImg/product4.png",
+    content: "Graphic design",
+    department: "English Department",
+  },
+  {
+    url: "/src/assets/productCardImg/product3.png",
+    content: "Graphic design",
+    department: "English Department",
+  },
+  {
+    url: "/src/assets/productCardImg/product2.png",
+    content: "Graphic design",
+    department: "English Department",
+  },
 ];
 export default productData;
