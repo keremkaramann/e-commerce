46ac3da1aad4cb723fa3dfa07c90fee0145d84d5
Kerem Karaman <keremkaraman91@gmail.com>
2023-10-17 11:56:37 +0300

    repetitive  added done

--- src/components/Repetitive/TeamAboutNavbar.jsx
+++ src/components/Repetitive/TeamAboutNavbar.jsx
@@ -1,7 +1,7 @@
 import { AiOutlineArrowRight } from "react-icons/ai";
 const TeamAboutNavbar = () => {
   return (
-    <div className="flex items-center p-7">
+    <div className="flex items-center p-7 justify-evenly">
       <div>
         <h1 className="font-bold text-2xl text-dark-navy">Bandage</h1>{" "}
       </div>
@@ -11,9 +11,11 @@ const TeamAboutNavbar = () => {
         <a href="/contact">Pricing</a>
         <a href="/contact">Contact</a>
       </div>
-      <div className="flex gap-5">
-        <a href="">Login</a>
-        <button className="flex items-center gap-4">
+      <div className="flex gap-10 items-center">
+        <a href="" className="text-sm font-bold text-primary-blue">
+          Login
+        </a>
+        <button className="flex items-center gap-4 text-sm font-bold rounded-l border-[1px] border-primary-blue text-white bg-primary-blue px-6 py-4 hover:text-primary-blue hover:bg-white duration-500">
           Become a member <AiOutlineArrowRight />
         </button>
       </div>
