4ea92176a04e0dcb247b12c66a4c91a7de2ec291
Kerem Karaman <keremkaraman91@gmail.com>
2023-10-19 16:41:00 +0300

    section2 done

--- src/components/Page/Product.jsx
+++ src/components/Page/Product.jsx
@@ -2,28 +2,46 @@ import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
 import Brands from "../Repetitive/Brands";
 import { BsChevronRight } from "react-icons/bs";
-import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import {
+  AiFillStar,
+  AiOutlineStar,
+  AiOutlineHeart,
+  AiFillEye,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 import Example from "../Repetitive/ProductCaoursel";
+import { useState } from "react";
 
 const Product = () => {
   const colorOptions = [
     {
       name: "Primary",
-      className: "bg-primary-blue color-picker",
+      className: "bg-primary-blue color-picker2",
     },
     {
       name: "Success",
-      className: "bg-success-color color-picker",
+      className: "bg-success-color color-picker2",
     },
     {
       name: "Alert",
-      className: "bg-alert-color color-picker",
+      className: "bg-alert-color color-picker2",
     },
     {
       name: "Navy",
-      className: "bg-dark-navy color-picker",
+      className: "bg-dark-navy color-picker2",
     },
   ];
+
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const handleColorClick = (color) => {
+    if (color === selectedColor) {
+      setSelectedColor(null);
+    } else {
+      setSelectedColor(color);
+    }
+  };
+
   return (
     <section>
       <Header justify={"justify-between"} />
@@ -41,11 +59,11 @@ const Product = () => {
           </div>
         </div>
       </div>
-      <div className="bg-[#FAFAFA] flex justify-center gap-10">
+      <div className="bg-[#FAFAFA] flex justify-center gap-20">
         <div>
           <Example />
         </div>
-        <div className="w-1/2">
+        <div className="w-[40%]">
           <h4 className="dark-navy text-lg mb-3 mt-3">Floating Phone</h4>
           <div className="flex items-center gap-2 text-2xl text-[#F3CD03] mb-4">
             <AiFillStar />
@@ -64,7 +82,28 @@ const Product = () => {
             met sent. RELIT official consequent door ENIM RELIT Mollie. <br />
             Excitation venial consequent sent nostrum met.
           </p>
-          <div className="border-b-2 border-muted-color w-3/4"></div>
+          <div className="border-b-2 border-muted-color w-3/4 mb-8"></div>
+          <ul className="flex gap-2 mb-20">
+            {colorOptions.map((colorOption, index) => (
+              <li
+                key={index}
+                className={`color-option ${colorOption.className} ${
+                  selectedColor === colorOption.name
+                    ? "border-solid border-2 border-black"
+                    : ""
+                }`}
+                onClick={() => handleColorClick(colorOption.name)}
+              ></li>
+            ))}
+          </ul>
+          <div className="flex items-center gap-3">
+            <button className="px-4 py-2 rounded border-[1px] bg-primary-blue border-primary-blue text-white hover:bg-white hover:text-primary-blue duration-500">
+              Select Options
+            </button>
+            <AiOutlineHeart className="addToChart" />
+            <AiOutlineShoppingCart className="addToChart" />
+            <AiFillEye className="addToChart" />
+          </div>
         </div>
       </div>
       <Brands />
