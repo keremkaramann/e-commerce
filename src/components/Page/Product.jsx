aa516cf567f1f6f302d8194945a385f9d34724a6
Kerem Karaman <keremkaraman91@gmail.com>
2023-10-19 13:41:04 +0300

    product nav done

--- src/components/Page/Product.jsx
+++ src/components/Page/Product.jsx
@@ -5,6 +5,23 @@ const Product = () => {
   return (
     <section>
       <Header />
+      <section className=" bg-[#FAFAFA] py-5">
+        <div className=" flex items-center justify-between mx-auto p-5 w-full max-w-[1200px] xs:flex-col mdCstm:flex-row">
+          <h1 className="text-dark-navy text-2xl font-bold xs:mb-10 mdCstm:mb-0">
+            Shop
+          </h1>
+          <div></div>
+          <div className="flex gap-3">
+            <a href="/" className="text-dark-navy text-sm font-bold">
+              Home
+            </a>
+            <BsChevronRight className="text-muted-color font-bold text-lg" />
+            <a href="/products" className="text-muted-color text-sm font-bold">
+              Shop
+            </a>
+          </div>
+        </div>
+      </section>
       <Brands />
       <Footer />
     </section>
