6f7e54c6bd3042979db01a1b8824c0adcb03c80d
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-01 00:17:19 +0300

    toastify fixed

--- src/store/actions/userAction.jsx
+++ src/store/actions/userAction.jsx
@@ -18,7 +18,7 @@ export const handleLogin = (data, history) => (dispatch) => {
       history.push("/");
       toast.success(`Welcome ${res.data.name}`, {
         position: "top-right",
-        autoClose: 5000,
+        autoClose: 3000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
