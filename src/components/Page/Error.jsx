ad0be7b0bcd8d3dc9e2d22cafd19a588e29992e3
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-02 17:16:47 +0300

    error page added

--- src/components/Page/Error.jsx
+++ src/components/Page/Error.jsx
@@ -1,4 +1,18 @@
+import Lottie from "lottie-react";
+import spaceError from "../../lottie/animation_loh9chq7.json";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="bg-dark-navy">
+      <div className="flex justify-center pt-8 mb-4">
+        <div>
+          <Link className="btn1Sm" to="/">
+            Go Back To Home Page
+          </Link>
+        </div>
+      </div>
+      <Lottie animationData={spaceError} loop={true} />
+    </div>
+  );
 };
 export default Error;
