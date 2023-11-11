b6f25b9d8cd5d5c3b5ec679ec78fd8743ff7f9c3
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-11 20:15:23 +0300

    material tailwind erased from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,15 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "../src/store/store";
-import { ThemeProvider } from "@material-tailwind/react";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <ThemeProvider>
-      <ToastContainer />
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
+    <ToastContainer />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
