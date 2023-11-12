f0052332decbbaca02610acd25ac4057718e67d1
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-12 10:27:56 +0300

    error page bg colo fixed

--- src/components/Page/Error.jsx
+++ src/components/Page/Error.jsx
@@ -3,7 +3,7 @@ import spaceError from "../../lottie/animation_loh9chq7.json";
 import { Link } from "react-router-dom";
 const Error = () => {
   return (
-    <div className="bg-dark-navy">
+    <div className="bg-dark-navy h-[1250px]">
       <div className="flex justify-center pt-8 mb-4">
         <div>
           <Link className="btn1Sm" to="/">
