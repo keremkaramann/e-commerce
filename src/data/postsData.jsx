812c3ca6e32aaee8d0822807600d578c6ed44595
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-24 20:43:11 +0300

    image path fixed postsData

--- src/data/postsData.jsx
+++ src/data/postsData.jsx
@@ -3,17 +3,17 @@ import second from "/src/assets/posts/unsplash_dEGu-oCuB1Y.png";
 import third from "/src/assets/posts/unsplash_tVEqStC2uz8.png";
 export const imagePosts = [
   {
-    url: { first },
+    url: first,
     content: "Graphic design",
     department: "English Department",
   },
   {
-    url: { second },
+    url: second,
     content: "Graphic design",
     department: "English Department",
   },
   {
-    url: { third },
+    url: third,
     content: "Graphic design",
     department: "English Department",
   },
