1fbe57c1cb26045ad0cd485189dc057bd65b42cd
Kerem Karaman <keremkaraman91@gmail.com>
2023-11-25 13:52:23 +0300

    img url fixed

--- src/components/Repetitive/Brands.jsx
+++ src/components/Repetitive/Brands.jsx
@@ -1,12 +1,11 @@
+import br1 from "/src/assets/brands/fa-brands-1.png";
+import br2 from "/src/assets/brands/fa-brands-2.png";
+import br3 from "/src/assets/brands/fa-brands-3.png";
+import br4 from "/src/assets/brands/fa-brands-4.png";
+import br5 from "/src/assets/brands/fa-brands-5.png";
+import br6 from "/src/assets/brands/fa-brands-6.png";
 const Brands = () => {
-  const imgBrands = [
-    "/src/assets/brands/fa-brands-1.png",
-    "/src/assets/brands/fa-brands-2.png",
-    "/src/assets/brands/fa-brands-3.png",
-    "/src/assets/brands/fa-brands-4.png",
-    "/src/assets/brands/fa-brands-5.png",
-    "/src/assets/brands/fa-brands-6.png",
-  ];
+  const imgBrands = [br1, br2, br3, br4, br5, br6];
   return (
     <div className="flex mdCstm:justify-around items-center bg-[#FAFAFA] p-28 flex-wrap xs:gap-12 middle:gap-8 xs:justify-center xs:flex-col middle:flex-row">
       {imgBrands.map((url, index) => {
--- src/data/bestseller.js
+++ src/data/bestseller.js
@@ -1,49 +1,54 @@
+import pr1 from "/src/assets/bestSellerImg/product-cover-5.png";
+import pr2 from "/src/assets/bestSellerImg/product-cover-5 (1).png";
+import pr3 from "/src/assets/bestSellerImg/product-cover-5 (2).png";
+import pr4 from "/src/assets/bestSellerImg/product-cover-5 (3).png";
+
 const bestSellerData = [
   {
     key: 1,
-    url: "/src/assets/bestSellerImg/product-cover-5.png",
+    url: pr1,
     content: "Graphic design",
     department: "English Department",
   },
   {
     key: 2,
-    url: "/src/assets/bestSellerImg/product-cover-5 (1).png",
+    url: pr2,
     content: "Graphic design",
     department: "English Department",
   },
   {
     key: 3,
-    url: "/src/assets/bestSellerImg/product-cover-5 (2).png",
+    url: pr3,
     content: "Graphic design",
     department: "English Department",
   },
   {
     key: 12,
-    url: "/src/assets/bestSellerImg/product-cover-5 (3).png",
+    url: pr4,
     content: "Graphic design",
     department: "English Department",
   },
   {
     key: 4,
-    url: "/src/assets/bestSellerImg/product-cover-5.png",
+    url: pr1,
     content: "Graphic design",
     department: "English Department",
   },
   {
     key: 5,
-    url: "/src/assets/bestSellerImg/product-cover-5 (1).png",
+    url: pr2,
     content: "Graphic design",
     department: "English Department",
   },
   {
     key: 6,
-    url: "/src/assets/bestSellerImg/product-cover-5 (2).png",
+    url: pr3,
     content: "Graphic design",
     department: "English Department",
   },
   {
     key: 7,
-    url: "/src/assets/bestSellerImg/product-cover-5 (3).png",
+    url: pr4,
     content: "Graphic design",
     department: "English Department",
   },
