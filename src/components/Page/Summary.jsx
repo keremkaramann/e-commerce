2dfef775d02d32f51f2477b69e3dfaa3fb82e485
Kerem Karaman <keremkaraman91@gmail.com>
2023-12-26 20:23:57 +0300

    OrderSummary added to summary page

--- src/components/Page/Summary.jsx
+++ src/components/Page/Summary.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Footer from "../Layout/Footer";
 import Header from "../Layout/Header";
 import { useSelector, useDispatch } from "react-redux";
@@ -6,19 +5,17 @@ import { BsFillTrash3Fill } from "react-icons/bs";
 import Lottie from "lottie-react";
 import emptyProduct from "../../lottie/noProduct.json";
 import { NavLink } from "react-router-dom";
-import { FiChevronsRight } from "react-icons/fi";
-import { FaShippingFast } from "react-icons/fa";
+
 import {
   removeCart,
   increaseCount,
   decreaseCount,
 } from "../../store/actions/shoppingCartAction";
+import OrderSummary from "../Repetitive/OrderSummary";
 
 const Summary = () => {
-  const [totalPrice, setTotalPrice] = useState(0);
   const cartItems = useSelector((store) => store.cart.cart);
   const dispatch = useDispatch();
-  const shippingCost = 50;
 
   //error page
   const NoProducts = () => {
@@ -44,16 +41,6 @@ const Summary = () => {
       </div>
     );
   };
-
-  useEffect(() => {
-    let sum = 0;
-
-    cartItems.forEach((item) => {
-      sum += item.product.price * item.count;
-    });
-
-    setTotalPrice(sum);
-  }, [cartItems]);
 
   return (
     <>
@@ -147,66 +134,7 @@ const Summary = () => {
               </NavLink>
             </div>
           </div>
-          <div className="flex items-start mt-20 xs-p-3 middle:p-0">
-            <div className="border-[1px] border-muted-color xs:w-[290px] middle:w-[400px] p-5 leading-10 text-dark-navy">
-              <h1 className="text-dark-navy font-bold text-xl mb-10 mt-2">
-                Order Summary
-              </h1>
-              <div className="flex justify-between gap-5 text-lg m-2">
-                <p className="font-medium">SubTotal:</p>
-                <p> ${totalPrice.toFixed(2)}</p>
-              </div>
-              <div className="flex justify-between gap-5 text-lg m-2">
-                <div className="flex flex-col mb-2">
-                  <p className="font-medium mb-2">Shipping:</p>
-                  <span className="font-thin text-secondary-text text-sm flex items-center gap-1">
-                    <FaShippingFast className="font-medium" />
-                    {totalPrice.toFixed(2) > 500
-                      ? "Free shipping over $500!"
-                      : `Only $${(500 - totalPrice).toFixed(
-                          2
-                        )} left for free shipping!`}
-                  </span>
-                </div>
-                <p>
-                  {totalPrice.toFixed(2) > 500
-                    ? "-$" + shippingCost
-                    : "$" + shippingCost}
-                </p>
-              </div>
-              <div className="flex text-lg m-2 flex-wrap xs:justify-center middle:justify-normal mb-5">
-                <p
-                  className=" text-primary-blue font-medium 
-                cursor-pointer border-[1px] bg-sky-200 xs:px-[3.3rem] xs:py-1 middle:py-1 middle:px-4"
-                >
-                  Apply
-                </p>
-                <input
-                  type="text"
-                  placeholder="Promo Code"
-                  className="border-[1px] p-1 xs:w-40 text-muted-color bg-slate-100"
-                />
-              </div>
-              <div className="flex justify-between gap-5 text-lg m-2 font-bold">
-                <p className="font-bold">Total:</p>
-                <p>
-                  $
-                  {totalPrice.toFixed(2) > 500
-                    ? (totalPrice - 50).toFixed(2)
-                    : (totalPrice + 50).toFixed(2)}
-                </p>
-              </div>
-              <NavLink to="/checkout">
-                <div
-                  className="flex items-center justify-center mt-5
-                gap-2 bg-dark-navy py-2 text-white font-medium"
-                >
-                  <button>CHECKOUT</button>
-                  <FiChevronsRight />
-                </div>
-              </NavLink>
-            </div>
-          </div>
+          <OrderSummary />
         </section>
       )}
       <Footer />
