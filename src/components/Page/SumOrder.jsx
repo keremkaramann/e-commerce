34841ca7fea899c8618b506e3404455807863e0a
Kerem Karaman <keremkaraman91@gmail.com>
2024-01-18 19:15:19 +0300

    sumorder page ui done

--- src/components/Page/SumOrder.jsx
+++ src/components/Page/SumOrder.jsx
@@ -1,9 +1,16 @@
-import confetti from "canvas-confetti";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import confetti from "canvas-confetti";
+import Lottie from "lottie-react";
+import orderChecked from "../../lottie/checkedAnimation.json";
+//pages
 import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
 
 const SumOrder = () => {
+  const user = useSelector((store) => store.user.user);
+
   var count = 200;
   var defaults = {
     origin: { y: 0.7 },
@@ -45,7 +52,32 @@ const SumOrder = () => {
   return (
     <>
       <Header />
-      <section className="mt-10 mb-10"></section>
+      <section className="pt-14 mb-14 flex justify-center">
+        <div className="shadow-2xl shadow-slate-700 p-10 text-center">
+          <Lottie
+            animationData={orderChecked}
+            style={{ height: 150 }}
+            loop={false}
+          />
+
+          <p className="font-medium text-xl mb-2">Hey {user?.name}, </p>
+          <p className="font-bold text-3xl mb-2">Your Order is Confirmed!</p>
+          <p className="font-medium text-xl">
+            We'll send you a shipping confirmation email
+          </p>
+          <p className="font-medium text-xl">as soon as your order ships.</p>
+          <div className="mt-5">
+            <NavLink to="/shopping" className="text-white font-medium text-end">
+              <p
+                className="bg-primary-blue hover:bg-dark-navy rounded-lg
+              ease-in-out duration-500 px-2 py-2 text-center"
+              >
+                Continue Shopping
+              </p>
+            </NavLink>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
